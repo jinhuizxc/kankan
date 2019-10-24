16c8c3b38a76cbb94627bcae79db0892435423be
1147471053@qq.com <1147471053@qq.com>
2019-10-24 23:22:21 +0800

    实现定位基本功能
    实现定位基本功能

--- app/src/main/java/com/ychong/kankan/map/baidu/BaiDuMapActivity.java
+++ app/src/main/java/com/ychong/kankan/map/baidu/BaiDuMapActivity.java
@@ -1,8 +1,10 @@
 package com.ychong.kankan.map.baidu;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -11,16 +13,34 @@ import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.ychong.kankan.R;
 import com.ychong.kankan.ui.BaseActivity;
+import com.ychong.kankan.utils.PermissionsChecker;
 
 public class BaiDuMapActivity extends BaseActivity {
     private LinearLayout mapLl;
     private MapView mapView;
     private BaiduMap mBaiduMap;
     private LocationClient mLocationClient;
+    private  boolean isFirstLoc = true;// 是否首次定位
+
+    private static final int REQUEST_CODE = 0; // 请求码
+
+    // 所需的全部权限
+    static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private PermissionsChecker mPermissionsChecker; // 权限检测器
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,6 +56,7 @@ public class BaiDuMapActivity extends BaseActivity {
     }
 
     private void initData() {
+        initPermissions();
         mapLl.addView(mapView);
         mBaiduMap = mapView.getMap();
         mBaiduMap.setMyLocationEnabled(true);
@@ -96,16 +117,38 @@ public class BaiDuMapActivity extends BaseActivity {
 
         @Override
         public void onReceiveLocation(BDLocation location) {
-            //mapView 销毁后不在处理新接收的位置
-            if (location == null || mapView == null){
+            // map view 销毁后不在处理新接收的位置
+            if (location == null || mapView == null) {
                 return;
             }
             MyLocationData locData = new MyLocationData.Builder()
                     .accuracy(location.getRadius())
                     // 此处设置开发者获取到的方向信息，顺时针0-360
-                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .direction(100).latitude(location.getLatitude())
                     .longitude(location.getLongitude()).build();
             mBaiduMap.setMyLocationData(locData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                // MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+                // 设置缩放比例,更新地图状态
+                float f = mBaiduMap.getMaxZoomLevel();// 19.0
+                MapStatusUpdate u = MapStatusUpdateFactory.newLatLngZoom(ll,
+                        f - 2);
+                mBaiduMap.animateMapStatus(u);
+                //地图位置显示
+                Toast.makeText(BaiDuMapActivity.this, location.getAddrStr(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+    public void initPermissions(){
+        mPermissionsChecker = new PermissionsChecker(this);
+        // 缺少权限时, 进入权限配置页面
+        if (mPermissionsChecker.lacksPermissions(PERMISSIONS)) {
+            PermissionsActivity.startPermissionsActivity(this,REQUEST_CODE,PERMISSIONS);
         }
     }
 }
--- app/src/main/java/com/ychong/kankan/utils/BaseContract.java
+++ app/src/main/java/com/ychong/kankan/utils/BaseContract.java
@@ -1,6 +1,6 @@
 package com.ychong.kankan.utils;
 
 public class BaseContract {
-    public static final String SERVER_HOST_URL = "http://192.168.0.106:10086/";
+    public static final String SERVER_HOST_URL = "http://192.168.0.100:10086/";
     public static final String FTP_HOST_URL = "http://47.98.147.72:9999/file/ychong/";
 }
